06593fc38bca64fa75b8011062f0b97ea309daa9
NGC3372 <rainbowinthesunshine@gmail.com>
2021-07-05 18:13:29 +0800

    routine

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -9,61 +9,71 @@ import 'beans/download_info.dart';
 // ignore: must_be_immutable
 class SearchPage extends StatelessWidget {
   TextEditingController searchController = TextEditingController();
-  RxList<DownloadInfo> searchedList = RxList<DownloadInfo>([]);
+  RxList<int> searchedList = RxList<int>([]);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-            onPressed: () {
-              Get.back();
-            },
-            icon: Icon(
-              Icons.arrow_back_ios,
-              color: Colors.black,
-            )),
-        title: TextField(
-          controller: searchController,
-          autofocus: true,
-          onChanged: (value) {
-            search(value);
-          },
-          decoration: InputDecoration(
-            border: null,
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              searchController.text = '';
-            },
-            icon: Icon(Icons.clear),
-            color: Colors.black,
-          )
-        ],
-      ),
-      body: Container(
-          color: Colors.grey,
-          child: Obx(
-            () => ListView.builder(
-              itemCount: searchedList.length,
-              itemBuilder: (context, index) {
-                return MyListTitle(index);
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+              onPressed: () {
+                Get.back();
               },
+              icon: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+              )),
+          title: TextField(
+            controller: searchController,
+            autofocus: true,
+            onChanged: (value) {
+              search(value);
+            },
+            decoration: InputDecoration(
+              border: null,
             ),
-          )),
-    );
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                searchController.text = '';
+              },
+              icon: Icon(Icons.clear),
+              color: Colors.black,
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Divider(),
+            Expanded(
+              child: Container(
+                  color: Colors.white,
+                  child: Obx(
+                    () => ListView.builder(
+                      itemCount: searchedList.length,
+                      itemBuilder: (context, index) {
+                        return MyListTitle(searchedList[index]);
+                      },
+                    ),
+                  )),
+            )
+          ],
+        ));
   }
 
   void search(String searchContent) {
-    List<DownloadInfo> tempList = [];
+    List<int> tempList = [];
+    searchedList.clear();
     if (searchContent != '') {
       for (var temp in Get.find<ControlHome>().downloadedInfo) {
-        if (temp.fileName.contains(searchContent)) tempList.add(temp);
+        if (temp.fileName.contains(searchContent)) {
+          tempList.add(Get.find<ControlHome>().downloadedInfo.indexOf(temp));
+        }
       }
     }
+
     searchedList.addAll(tempList);
   }
 }
